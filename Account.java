35871c6cc0022dee6510d2418e2c51d25b6c4566
Claus Bové <clbo@kea.dk>
2017-03-23 20:07:15 +0100

    Code added

---
+++ Account.java
@@ -0,0 +1,23 @@
+public class Account {
+
+	private int accountNumber;
+	private double balance;
+
+	public void deposit() {
+		// TODO - implement Account.deposit	
+	}
+
+	public void withdraw(int amount) {
+		// TODO - implement Account.withdraw	
+	}
+
+	public int getAccountNumber() {
+		return this.accountNumber;
+	}
+
+	public double checkBalance() {
+		// TODO - implement Account.checkBalance
+		return this.balance;
+	}
+
+}
---
+++ Customer.java
@@ -0,0 +1,10 @@
+public class Customer {
+
+	private String name;
+	private Account[] accounts;
+
+	public String getName() {
+		return this.name;
+	}
+
+}
